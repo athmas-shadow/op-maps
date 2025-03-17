4cbe06df04ebf0b0f6e6f5a64c2d311c972fda52
athini <24695424@sun.ac.za>
2025-03-17 21:14:06 +0200

    restructure and code setup for red black tree

---
+++ src/rbtree.h
@@ -0,0 +1,30 @@
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
+enum colour_t { BLACK, RED };
+
+struct rb_node_t {
+  struct rb_node_t *parent;
+  struct rb_node_t *left;
+  struct rb_node_t *right;
+  enum colour_t colour;
+  void *value;
+};
+
+struct rb_tree {
+  struct rb_node_t *root;
+
+  //current node in iteration.
+  struct rb_node_t *current;
+  
+  int (*cmp)(void *n1, void *n2);
+  int (*find)(void *val);
+  
+};
+
+struct rb_tree *rb_create_tree();
+void rb_init_iterator(struct rb_tree *tree);
+bool rb_has_next(struct rb_tree *tree);
+void rb_next(struct rb_tree *tree);
+
+
+#endif
