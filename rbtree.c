a9dd958512d274e7c72bb4cc2e3c1c99e076a080
athini <24695424@sun.ac.za>
2025-03-17 18:32:58 +0200

    initial commit

---
+++ rbtree.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "rbtree.h"
+
+
+
+
+
+static void insert(struct rb_tree *tree, void *val)
+{
+
+}
+
+static void delete(struct rb_tree *tree, void *val)
+{
+
+}
+
+int main(void) {
+  printf("header file works.\n");
+  return 0;
+}
